f0bd695e96d8b863b3eb3c86fddef045290c7255
Juan Salazar <ferthelet@gmail.com>
2023-03-25 01:17:33 -0500

    adds functions headers to start working

---
+++ index.js
@@ -0,0 +1,23 @@
+// js
+
+function receivesAFunction() {
+    // take a callback function as an argument
+    // call the callback function
+    // it doesn't matter what this function returns, so long as it calls
+    // the callback function
+    return 0;
+}
+
+function returnsANamedFunction() {
+    // take no arguments
+    // return a named function
+    return 0;
+}
+
+function returnsAnAnonymousFunction() {
+    // take no arguments
+    // return an anonymous function
+    return 0;
+}
+
+
